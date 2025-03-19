c49f1417674427a9e2d65ca88ef87a0d39d8a79c
jihyuuk_home <jihyuk.dev@gmail.com>
2025-03-20 04:40:31 +0900

    first commit

---
+++ src/main/java/com/jihyuk/mojong_v2/MojongV2Application.java
@@ -0,0 +1,13 @@
+package com.jihyuk.mojong_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojongV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojongV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jihyuk/mojong_v2/MojongV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihyuk.mojong_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojongV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
